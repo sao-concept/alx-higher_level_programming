3ace5a87db4a995d1ae1c24f66f0008a675eb732
Azeez Saka <saoconcept1@gmail.com>
2023-09-05 04:30:15 -0700

    add 11-pow.py file

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def power(base, exponent):
+    """
+    Calculate the power of a number raised to an exponent.
+
+    Args:
+        base (int or float): The base number.
+        exponent (int or float): The exponent.
+
+    Returns:
+        int or float: The result of 'base' raised to the power of 'exponent'.
+    """
+    return(base ** exponent)
+
+# Example usage:
+# result = power(2, 3)  # Returns 8
