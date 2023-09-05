63cb5796b2e4cc7a9f3c49915b019972fa8b5c5c
Azeez Saka <saoconcept1@gmail.com>
2023-09-05 04:27:37 -0700

    add 10-add.py file

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add(a, b):
+    """
+    Add two numbers and return the result.
+
+    Args:
+        a (int or float): The first number.
+        b (int or float): The second number.
+
+    Returns:
+        int or float: The result of adding 'a' and 'b'.
+    """
+    return (a + b)
+
+# Example usage:
+# result = add(5, 3)  # Returns 8
