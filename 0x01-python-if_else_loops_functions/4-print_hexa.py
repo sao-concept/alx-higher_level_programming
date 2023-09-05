fe8bbfcbf9061316c3d57245d322fc0e5e9295f2
Azeez Saka <saoconcept1@gmail.com>
2023-09-05 04:07:10 -0700

    add 4-print_hexa.py file

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# Loop through numbers from 0 to 98 (99 exclusive).
+for num in range(99):
+    # Print the number in decimal and hexadecimal format.
+    print('{} = 0x{:x}'.format(num, num))
+
+# This code prints numbers from 0 to 98 in decimal and hexadecimal format.
