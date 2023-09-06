444726536ea81cb079da05b1906868f4758dabd6
Azeez Saka <saoconcept1@gmail.com>
2023-09-06 02:47:47 -0700

    modify 2-print_alphabet.py file

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-# Loop through ASCII values for lowercase letters 'a' to 'z'.
 for ascii_value in range(ord('a'), ord('z') + 1):
     print('{:c}'.format(ascii_value), end='')
-# This code prints all lowercase letters from 'a' to 'z' without a newline.
