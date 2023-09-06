78ad172a24bb0450c9d9ad3401d6cc8d84522913
Azeez Saka <saoconcept1@gmail.com>
2023-09-06 03:23:32 -0700

    modify and grant permission to 100-print_tebahpla.py file

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-# Loop through ASCII values for lowercase letters 'z' to 'a' in reverse order.
-for i in range(ord('z'), ord('a') - 1, -1):
-    # Calculate the difference to adjust the ASCII value based on parity.
-    if i % 2 == 0:
+for letter in range(ord('z'), ord('a') - 1, -1):
+    if letter % 2 == 0:
         diff = 0
     else:
         diff = 32
-    # Print the character adjusted by the difference without a newline.
-    print('{}'.format(chr(i - diff)), end='')
-
-# This code prints alternating lowercase letters from 'z' to 'a'.
+    print('{}'.format(chr(letter - diff)), end='')
