08d24db284025be93e1fa0a78e7ac816807e569a
Azeez Saka <saoconcept1@gmail.com>
2023-09-06 03:10:16 -0700

    modify and grant permission to 6-print_comb3.py file

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-# Loop through numbers from 0 to 9 for 'x'.
-for x in range(10):
-    # Loop through numbers from 'x + 1' to 9 for 'y'.
+for x in range(0, 10):
     for y in range(x + 1, 10):
-        # Check if 'x' and 'y' are 8 and 9 respectively.
         if x == 8 and y == 9:
             print('89')
         else:
-            # Print 'x' and 'y' with a comma, except for the last pair.
             print('{}{}, '.format(x, y), end='')
-
-# This code prints combinations of two numbers from 01 to 89.
