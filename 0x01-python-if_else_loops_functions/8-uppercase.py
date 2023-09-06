a2f492334b78e4d04a0c2b898f4bb50b8e9ae8eb
Azeez Saka <saoconcept1@gmail.com>
2023-09-06 03:15:59 -0700

    modify 8-uppercase.py file

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,21 +1,7 @@
 #!/usr/bin/python3
-def uppercase(input_str):
-    """
-    Convert lowercase letters in a string to uppercase.
-
-    Args:
-        input_str (str): The input string to convert.
-
-    Returns:
-        str: The input string with lowercase letters converted to uppercase.
-    """
-    result = ""
-    for char in input_str:
-        if 97 <= ord(char) <= 122:
-            result += chr(ord(char) - 32)
-        else:
-            result += char
-    return result
-
-# Example usage:
-# result = uppercase("Hello World")  # Returns "HELLO WORLD"
+def uppercase(str):
+    for letter in str:
+        if ord(letter) >= 97 and ord(letter) <= 122:
+            letter = chr(ord(letter) - 32)
+        print("{}".format(letter), end="")
+    print()
