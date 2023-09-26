f526151379426eb7f37487800947389a1db1e462
Azeez Saka <saoconcept1@gmail.com>
2023-09-26 12:26:11 +0100

    add 1-square.py script

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initialize the Square class with a given size.
+
+        Args:
+            size (int): The size of the square.
+
+        Attributes:
+            __size (int): The size of the square.
+
+        """
+        self.__size = size
