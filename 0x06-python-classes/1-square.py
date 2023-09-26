8ac5713d8cbd5943e99875cb2ed37bbe80601e5d
Azeez Saka <saoconcept1@gmail.com>
2023-09-26 12:29:15 +0100

    modify 1-square.py script

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
+# 0-square.py by Azeez Saka
+"""Defines a square """
+
 
 class Square:
     """Represents a square"""
 
     def __init__(self, size):
-        """Initialize the Square class with a given size.
-
-        Args:
-            size (int): The size of the square.
-
-        Attributes:
-            __size (int): The size of the square.
-
+        """Initializing this square class
+        Args: size - represnets the size of the square defined
         """
-        self.__size = size
