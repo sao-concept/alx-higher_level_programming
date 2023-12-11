56c3b3e7b497eff12ab2e2a3958bc3e1c742f98e
Azeez Saka <saoconcept1@gmail.com>
2023-12-11 11:01:14 +0100

    modify 1-multi_languages.js script

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
 
-// Script that prints "JavaScript is amazing".
+/*
+ This script prints 3 lines:
+ The first line: “C is fun”
+ The second line: “Python is cool”
+ The third line: “JavaScript is amazing”
+*/
 
 const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
 console.log(myVar);
