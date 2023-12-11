af86706012c193207ea3b727b24f5203676994dd
Azeez Saka <saoconcept1@gmail.com>
2023-12-11 12:57:09 +0100

    add 10-factorial.js script

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// This script computes and prints a factorial
+
+function factorial (num) {
+  if (num === 0 || isNaN(num)) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
+
+console.log(factorial(parseInt(process.argv[2])));
